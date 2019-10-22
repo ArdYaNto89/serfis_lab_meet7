146b9f4bbe1f62db6f38ef1fe258600fa2bfb963
Indra _ARDIyanto <mulut.pahit.27@gmail.com>
2019-10-22 14:46:08 +0700

    Initial commit

---
+++ app/src/main/java/com/uty/serfis_lab_meet7/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.uty.serfis_lab_meet7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+import android.media.MediaPlayer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //button facebook jika di klik akan menampilkan halaman facebook.com
+        btn_facebook.setOnClickListener({
+            val i=Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://www.facebook.com/langitinspirasi"))
+            startActivity(i)
+        })
+
+        //button instagram jika di klik akan menampilkan halaman instagram.com
+        btn_instagram.setOnClickListener({
+            val i=Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://www.instagram.com/langitinspirasi.co.id"))
+            startActivity(i)
+        })
+
+        //mendeklarasi variabel media player dan memanggil data mp3
+        var MediaPlayer:MediaPlayer?=MediaPlayer.create(this,R.raw.black)
+
+        //button yang berfungsi untuk melakukan mp3
+        btn_play.setOnClickListener{
+            MediaPlayer?.start()
+        }
+        //button yang berfungsi untuk melakukan pause mp3
+        btn_pause.setOnClickListener{
+            MediaPlayer?.pause()
+        }
+        //button yang berfungsi untuk melakukan stop
+        btn_stop.setOnClickListener {
+            MediaPlayer?.pause()
+            MediaPlayer?.seekTo(0)
+            MediaPlayer?.stop()
+        }
+    }
+}
